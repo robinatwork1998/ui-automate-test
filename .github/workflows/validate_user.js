25e95439ea8e9d0eb3ae446579e2708c735917b1
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2022-05-16 21:36:20 +0530

    Update validate_user.js

--- .github/workflows/validate_user.js
+++ .github/workflows/validate_user.js
@@ -4,11 +4,11 @@ const user_lists = argv[3];
 
 if (!(user_lists && user_lists.length)) {
 	console.log('Invalid user list. Please specify list of allowed users.');
-	process.exit(0);
+	process.exit(1);
 } else {
 	if (!(user_lists.indexOf(user) > -1)) {
 		console.log("Invalid User. You don't have permission to run this workflow...");
-		process.exit(0);
+		process.exit(1);
 	} else {
 		console.log('Valid User');
 	}
