9fbc34533fe87bbfe0c3866d2a9af1baed62ed2d
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2022-05-20 17:07:59 +0530

    Update greet.js

--- greet.js
+++ greet.js
@@ -1,4 +1,5 @@
 const argv = require('minimist')(process.argv.slice(2));
 
-console.log("This is a basic github action file")
+console.log("This is a basic github action file");
+console.log("This is a basic github action file");
 console.log(process.env)
